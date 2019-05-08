553929b6b40e12600cceff8575f82ba7a484face
Marcus Eduardo <marcusedu7@gmail.com>
2019-05-08 16:07:44 -0300

    PublicationDataSource - abstract class implemented

---
+++ lib/data/publication/publication_data_source.dart
@@ -0,0 +1,58 @@
+import 'package:meta/meta.dart';
+import 'package:res_publica/model/publication_entity.dart';
+import 'package:res_publica/model/react_entity.dart';
+import 'package:res_publica/model/reply_entity.dart';
+import 'package:res_publica/model/request_response.dart';
+
+abstract class PublicationDataSource {
+  //-------Publication-------
+  Future<RequestResponse<PublicationEntity>> publish(
+      PublicationEntity publication);
+
+  Future<RequestResponse<PublicationEntity>> updatePublication(
+      PublicationEntity publication);
+
+  Future<RequestResponse<PublicationEntity>> deletePublication(
+      PublicationEntity publication);
+
+  //-------React in Publication-------
+  Future<RequestResponse<ReactEntity>> reactInPublication(ReactEntity react);
+
+  Future<RequestResponse<ReactEntity>> unReactInPublication(
+      String publicationId, String userId);
+
+  //-------Reply to Publication-------
+  Future<RequestResponse<ReplyEntity>> replyToPublication(ReplyEntity reply);
+
+  Future<RequestResponse<ReplyEntity>> updateReplyToPublication(
+      ReplyEntity reply);
+
+  Future<RequestResponse<ReplyEntity>> removeReplyToPublication(
+      ReplyEntity reply);
+
+  //-------React in Reply-------
+  Future<RequestResponse<ReactEntity>> reactInReply(ReactEntity react);
+
+  Future<RequestResponse<ReactEntity>> unReactInReply(
+      String publicationId, String userId);
+
+  //-------Feed-------
+  Future<RequestResponse<List<PublicationEntity>>> feed({
+    @required int page,
+    @required int itemPerPage,
+  });
+
+  //-------Publication that I followed-------
+  Future<RequestResponse<List<PublicationEntity>>> publicationsFollowed(
+    String userId, {
+    @required int page,
+    @required int itemPerPage,
+  });
+
+  //-------My Publication-------
+  Future<RequestResponse<List<PublicationEntity>>> myPublications(
+    String userId, {
+    @required int page,
+    @required int itemPerPage,
+  });
+}
