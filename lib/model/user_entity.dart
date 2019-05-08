c9f5302b9549426006d7c7aa5583d564c481806c
Marcus Eduardo <marcusedu7@gmail.com>
2019-05-08 14:51:33 -0300

    UserEntity - implemented

---
+++ lib/model/user_entity.dart
@@ -0,0 +1,44 @@
+class UserEntity {
+  String password;
+  String updatedAt;
+  String userId;
+  String name;
+  String cpf;
+  String photo;
+  String createdAt;
+  String email;
+
+  UserEntity(
+      {this.password,
+      this.updatedAt,
+      this.userId,
+      this.name,
+      this.cpf,
+      this.photo,
+      this.createdAt,
+      this.email});
+
+  UserEntity.fromJson(Map<String, dynamic> json) {
+    password = json['password'];
+    updatedAt = json['updated_at'];
+    userId = json['user_id'];
+    name = json['name'];
+    cpf = json['cpf'];
+    photo = json['photo'];
+    createdAt = json['created_at'];
+    email = json['email'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['password'] = this.password;
+    data['updated_at'] = this.updatedAt;
+    data['user_id'] = this.userId;
+    data['name'] = this.name;
+    data['cpf'] = this.cpf;
+    data['photo'] = this.photo;
+    data['created_at'] = this.createdAt;
+    data['email'] = this.email;
+    return data..removeWhere((_, value) => value == null);
+  }
+}
