e2c3d510bd61241f7c023b33f67bd9734bd43aaa
Marcus Eduardo <marcusedu7@gmail.com>
2019-05-08 15:01:03 -0300

    AccountDataSource - implemented

---
+++ lib/data/account/remote/firebase_account_data_source.dart
@@ -0,0 +1,97 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:res_publica/data/account/account_data_source.dart';
+import 'package:res_publica/model/user_entity.dart';
+
+class FirebaseAccountDataSource implements AccountDataSource {
+  final FirebaseAuth fAuth;
+  final Firestore fStore;
+  final FirebaseStorage fStorage;
+
+  FirebaseAccountDataSource(this.fAuth, this.fStore, this.fStorage);
+
+  @override
+  Future<void> signOut() {
+    return fAuth.signOut();
+  }
+
+  @override
+  Future<UserEntity> register(String name, String email, String password) {
+    return fAuth
+        .createUserWithEmailAndPassword(email: email, password: password)
+        .asStream()
+        .map((fUser) {
+      fUser.updateProfile(UserUpdateInfo()..displayName = name);
+      return _adapterFirebaseUserToUser(fUser);
+    }).first;
+  }
+
+  @override
+  Future<void> resetPassword(String email) {
+    return fAuth.sendPasswordResetEmail(email: email);
+  }
+
+  @override
+  Future<UserEntity> signIn(String email, String password) async {
+    var fUser = await fAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+    return _adapterFirebaseUserToUser(fUser);
+  }
+
+  @override
+  Future<UserEntity> updateAccount(UserEntity user) async {
+    var fUserTemp = await fAuth.currentUser().then((fuser) {
+      fuser.updateProfile(UserUpdateInfo()
+        ..displayName = user.name
+        ..photoUrl = user.photo);
+    });
+    return _adapterFirebaseUserToUser(fUserTemp);
+  }
+
+  @override
+  Future<UserEntity> updatePassword(
+      String oldPassword, String newPassword) async {
+    var user = await fAuth.currentUser();
+    await user.updatePassword(newPassword);
+    return _adapterFirebaseUserToUser(user);
+  }
+
+  UserEntity _adapterFirebaseUserToUser(FirebaseUser fUser) {
+    if (fUser == null) return null;
+    var user = UserEntity();
+    user
+      ..userId = fUser.uid
+      ..name = fUser.displayName
+      ..email = fUser.email
+      ..photo = fUser.photoUrl
+      ..createdAt =
+          DateTime.fromMillisecondsSinceEpoch(fUser.metadata.creationTimestamp)
+              .toIso8601String();
+    return user;
+  }
+
+  @override
+  Future<UserEntity> signInWithGoogle(GoogleSignInAccount currentUser) async {
+    var userGoogleAuthentication = await currentUser.authentication;
+    var user = _adapterFirebaseUserToUser(await fAuth.linkWithCredential(
+        GoogleAuthProvider.getCredential(
+            idToken: userGoogleAuthentication.idToken,
+            accessToken: userGoogleAuthentication.accessToken)));
+    return user;
+  }
+
+  @override
+  Future<UserEntity> get currentUser async {
+    var u = await fAuth.currentUser();
+    return _adapterFirebaseUserToUser(u);
+  }
+
+  @override
+  Future<bool> hasEmailAccount(String text) async {
+    return null;
+  }
+}
