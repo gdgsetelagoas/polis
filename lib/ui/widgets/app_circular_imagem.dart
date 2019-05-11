f1b7e62584dc9e6aed5954cecb839bcc93403067
Marcus Eduardo <marcusedu7@gmail.com>
2019-05-11 01:42:25 -0300

    AppCircularImage - widget implemented

---
+++ lib/ui/widgets/app_circular_imagem.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+final urlRegex = RegExp(
+    r"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$");
+
+class AppCircularImage extends StatelessWidget {
+  final String path;
+  final double size;
+  final BoxFit fit;
+  final BorderSide borderSide;
+  final List<BoxShadow> shadows;
+
+  const AppCircularImage(this.path,
+      {Key key,
+      this.size = 75.0,
+      this.fit = BoxFit.cover,
+      this.borderSide,
+      this.shadows})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: ShapeDecoration(
+          shadows: shadows,
+          shape: CircleBorder(side: borderSide),
+          image: DecorationImage(
+              image: urlRegex.hasMatch(path)
+                  ? NetworkImage(path)
+                  : FileImage(File(path)),
+              fit: fit)),
+    );
+  }
+}
