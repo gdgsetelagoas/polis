b91fc46dc79e61f3178a9ba8d97c88b7fdd52502
Marcus Eduardo <marcusedu7@gmail.com>
2019-05-16 14:37:56 -0300

    AccountDataSource - update return type

--- lib/data/account/account_data_source.dart
+++ lib/data/account/account_data_source.dart
@@ -1,22 +1,25 @@
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:res_publica/model/request_response.dart';
 import 'package:res_publica/model/user_entity.dart';
 
 abstract class AccountDataSource {
-  Future<UserEntity> register(String name, String email, String password);
+  Future<RequestResponse<UserEntity>> register(UserEntity user);
 
-  Future<UserEntity> signIn(String email, String password);
+  Future<RequestResponse<UserEntity>> signIn(String email, String password);
 
-  Future<void> resetPassword(String email);
+  Future<RequestResponse> resetPassword(String email);
 
-  Future<UserEntity> updatePassword(String oldPassword, String newPassword);
+  Future<RequestResponse<UserEntity>> updatePassword(
+      String oldPassword, String newPassword);
 
-  Future<UserEntity> updateAccount(UserEntity user);
+  Future<RequestResponse<UserEntity>> updateAccount(UserEntity user);
 
-  Future<void> signOut();
+  Future<RequestResponse> signOut();
 
-  Future<UserEntity> signInWithGoogle(GoogleSignInAccount currentUser);
+  Future<RequestResponse<UserEntity>> signInWithGoogle(
+      GoogleSignInAccount currentUser);
 
   Future<UserEntity> get currentUser;
 
-  Future<bool> hasEmailAccount(String text);
+  Future<RequestResponse<bool>> hasEmailAccount(String text);
 }
