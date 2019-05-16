c2a80b7f25eaeed08fedb5fa21c2516a65ce4cf2
Marcus Eduardo <marcusedu7@gmail.com>
2019-05-16 17:22:56 -0300

    ProfileNoSignedWidget - message update and add icon

--- lib/ui/profile/widgets/profile_no_signed_widget.dart
+++ lib/ui/profile/widgets/profile_no_signed_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:res_publica/ui/profile/bloc/profile_bloc.dart';
 import 'package:res_publica/ui/profile/bloc/profile_events.dart';
 
@@ -13,10 +14,21 @@ class ProfileNoSignedWidget extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            Text(
-              "Você não está logado vacilão, fica esperto vai em login e se autentica!",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 18.0),
+            Opacity(
+              opacity: 0.75,
+              child: Icon(
+                FontAwesomeIcons.userSecret,
+                size: 96.0,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                "Você não está logado!.\nClica aqui em login e se autentica!",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18.0),
+              ),
             ),
             OutlineButton(
               onPressed: () {
