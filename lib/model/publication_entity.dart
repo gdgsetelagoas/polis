b0bfef27dc2e11626ad7b502ecb81c54733a2ba8
Marcus Eduardo <marcusedu7@gmail.com>
2019-05-19 17:52:20 -0300

    PublicationEntity - refactored

--- lib/model/publication_entity.dart
+++ lib/model/publication_entity.dart
@@ -1,11 +1,11 @@
 class PublicationEntity {
   String publicationId;
   int numViews;
-  String updatedAt;
+  DateTime updatedAt;
   String userId;
   bool edited;
   String subtitle;
-  String createdAt;
+  DateTime createdAt;
   List<PublicationResource> resources;
   bool checked;
   int numReplies;
@@ -15,27 +15,27 @@ class PublicationEntity {
 
   PublicationEntity(
       {this.publicationId,
-      this.numViews,
+      this.numViews = 0,
       this.updatedAt,
       this.userId,
-      this.edited,
+      this.edited = false,
       this.subtitle,
       this.createdAt,
       this.resources,
-      this.checked,
-      this.numReplies,
-      this.numReacts,
-      this.numFollowers,
+      this.checked = false,
+      this.numReplies = 0,
+      this.numReacts = 0,
+      this.numFollowers = 0,
       this.status});
 
   PublicationEntity.fromJson(Map<String, dynamic> json) {
     publicationId = json['publication_id'];
     numViews = json['num_views'];
-    updatedAt = json['updated_at'];
+    updatedAt = DateTime.tryParse(json['updated_at']);
     userId = json['user_id'];
     edited = json['edited'];
     subtitle = json['subtitle'];
-    createdAt = json['created_at'];
+    createdAt = DateTime.tryParse(json['created_at']);
     if (json['resources'] != null) {
       resources = new List<PublicationResource>();
       (json['resources'] as List).forEach((v) {
@@ -53,11 +53,11 @@ class PublicationEntity {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['publication_id'] = this.publicationId;
     data['num_views'] = this.numViews;
-    data['updated_at'] = this.updatedAt;
+    data['updated_at'] = this.updatedAt.toIso8601String();
     data['user_id'] = this.userId;
     data['edited'] = this.edited;
     data['subtitle'] = this.subtitle;
-    data['created_at'] = this.createdAt;
+    data['created_at'] = this.createdAt.toIso8601String();
     if (this.resources != null) {
       data['resources'] = this.resources.map((v) => v.toJson()).toList();
     }
@@ -72,19 +72,22 @@ class PublicationEntity {
 
 class PublicationResource {
   String resource;
-  String type;
+  PublicationResourceType type;
 
   PublicationResource({this.resource, this.type});
 
   PublicationResource.fromJson(Map<String, dynamic> json) {
     resource = json['resource'];
-    type = json['type'];
+    type = PublicationResourceType.values
+        .firstWhere((e) => e.toString() == json['type'], orElse: () => null);
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['resource'] = this.resource;
-    data['type'] = this.type;
+    data['type'] = this.type.toString();
     return data..removeWhere((_, value) => value == null);
   }
 }
+
+enum PublicationResourceType { IMAGE, VIDEO }
