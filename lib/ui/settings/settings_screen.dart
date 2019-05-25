18716785d44e7b3c7c3f567e252c7fb759818796
Marcus Eduardo <marcusedu7@gmail.com>
2019-05-24 22:15:05 -0300

    Settings - terms and policy implemented

--- lib/ui/settings/settings_screen.dart
+++ lib/ui/settings/settings_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:res_publica/ui/widgets/app_errors_dialog.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'bloc/bloc.dart';
 
@@ -45,12 +46,39 @@ class SettingsScreen extends StatelessWidget {
                       Spacer(),
                       Text(
                         "Sair",
-                        style: TextStyle(color: Colors.grey.shade700, fontSize: 18.0),
+                        style: TextStyle(
+                            color: Colors.grey.shade700, fontSize: 18.0),
                       ),
                     ],
                   )),
             ),
-            Divider()
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: FlatButton(
+                  onPressed: () async {
+                    if (await canLaunch(
+                        "https://firebasestorage.googleapis.com/v0/b/res-publica-76d21.appspot.com/o/terms%2Fterms-v1.pdf?alt=media&token=1daa72d5-acdb-4c10-8916-55888dedc220")) {
+                      launch(
+                          "https://firebasestorage.googleapis.com/v0/b/res-publica-76d21.appspot.com/o/terms%2Fterms-v1.pdf?alt=media&token=1daa72d5-acdb-4c10-8916-55888dedc220");
+                    }
+                  },
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        FontAwesomeIcons.fileContract,
+                        color: Colors.grey.shade700,
+                      ),
+                      Spacer(),
+                      Text(
+                        "Termos de Uso",
+                        style: TextStyle(
+                            color: Colors.grey.shade700, fontSize: 18.0),
+                      ),
+                    ],
+                  )),
+            ),
+            Divider(),
           ],
         ),
       ),
