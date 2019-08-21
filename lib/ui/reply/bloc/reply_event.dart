1a323aa4378eebae8a88ebe8170723ca1902cbc3
marcus duarte <marcusedu@hotmail.com>
2019-08-21 16:08:39 -0300

    ReplyBloc - implementado

--- lib/ui/reply/bloc/reply_event.dart
+++ lib/ui/reply/bloc/reply_event.dart
@@ -1,4 +1,20 @@
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import 'package:res_publica/model/reply_entity.dart';
 
 @immutable
-abstract class ReplyEvent {}
+abstract class ReplyEvent extends Equatable {
+  ReplyEvent([List props = const []]) : super(props);
+}
+
+class ReplyLoadReactForReplyStatus extends ReplyEvent {
+  final ReplyEntity reply;
+
+  ReplyLoadReactForReplyStatus({@required this.reply}) : super([reply]);
+}
+
+class ReplyLoadUserData extends ReplyEvent {
+  final String userId;
+
+  ReplyLoadUserData({@required this.userId}) : super([userId]);
+}
